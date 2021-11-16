e35de6388301ecf4274474c39b8e98d3bca9e770
Evan Ai <89574347+evanai@users.noreply.github.com>
2021-11-16 09:26:52 -0800

    Update main.java

--- main.java
+++ main.java
@@ -29,7 +29,7 @@ public class Main {
         teams.put("chiefs", "6-4");
         teams.put("raiders", "5-4");
         teams.put("chargers", "5-4");
-        teams.put("rams", "7-2");
+        teams.put("rams", "7-3");
         teams.put("dolphins", "3-7");
         teams.put("vikings", "4-5");
         teams.put("patriots", "6-4");
@@ -38,7 +38,7 @@ public class Main {
         teams.put("jets", "2-7");
         teams.put("eagles", "4-6");
         teams.put("steelers", "5-3-1");
-        teams.put("49ers", "3-5");
+        teams.put("49ers", "4-5");
         teams.put("seahawks", "3-6");
         teams.put("buccaneers", "6-3");
         teams.put("titans", "8-2");
@@ -57,7 +57,7 @@ public class Main {
                 }
             }
             if (resp.contains("nfc")) {
-                System.out.println("NFC Standings:\n1. Green Bay Packers (8-2)\n2. Arizona Cardinals (8-2)\n3. Dallas Cowboys (7-2)\n4. Los Angeles Rams (7-2)\n5. Tampa Bay Buccaneers (6-3)\n6. New Orleans Saints (5-4)\n7. Carolina Panthers (5-5)\n8. Minnesota Vikings (4-5)\n9. Atlanta Falcons (4-5)\n10. Philadelphia Eagles (4-6)\n11. San Francisco 49ers (3-5)\n12. Washington Football Team (3-6)\n13. New York Giants (3-6)\n14. Seattle Seahawks (3-6)\n15. Chicago Bears (3-6)\n16. Detroit Lions (0-8-1)");
+                System.out.println("NFC Standings:\n1. Green Bay Packers (8-2)\n2. Arizona Cardinals (8-2)\n3. Dallas Cowboys (7-2)\n4. Los Angeles Rams (7-3)\n5. Tampa Bay Buccaneers (6-3)\n6. New Orleans Saints (5-4)\n7. Carolina Panthers (5-5)\n8. Minnesota Vikings (4-5)\n9. San Francisco 49ers (4-5)\n10. Atlanta Falcons (4-5)\n11. Philadelphia Eagles (4-6)\n12. Washington Football Team (3-6)\n13. New York Giants (3-6)\n14. Seattle Seahawks (3-6)\n15. Chicago Bears (3-6)\n16. Detroit Lions (0-8-1)");
             } 
             else if (resp.contains("afc")) {
                 System.out.println("AFC Standings:\n1. Tennessee Titans (8-2)\n2. Buffalo Bills (6-3)\n3. Baltimore Ravens (6-3)\n4. Pittsburgh Steelers (5-3-1)\n5. Kansas City Chiefs (6-4)\n6. New England Patriots (6-4)\n7. Los Angeles Chargers (5-4)\n8. Las Vegas Raiders (5-4))\n9. Cincinnati Bengals (5-4)\n10. Indianapolis Colts (5-5)\n11. Cleveland Browns (5-5)\n12. Denver Broncos (5-5)\n13. Miami Dolphins (3-7)\n14. New York Jets (2-7)\n15. Jacksonville Jaguars (2-7)\n16. Houston Texans (1-8)");
